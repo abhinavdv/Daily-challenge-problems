70a1eb67ac3b31ac9268f9274523e83c76706d56
abhinavdv <dvabhinav31@gmail.com>
2021-03-03 09:23:08 +0530

    Create SortColors.java

---
+++ SortColors.java
@@ -0,0 +1,60 @@
+//https://leetcode.com/problems/sort-colors/
+
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+// Example 2:
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+// Example 3:
+
+// Input: nums = [0]
+// Output: [0]
+// Example 4:
+
+// Input: nums = [1]
+// Output: [1]
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is 0, 1, or 2.
+
+
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int redPointer = 0, whitePointer = 0, bluePointer = nums.length - 1;
+        while(whitePointer<=bluePointer){
+            if(nums[whitePointer]==2){
+                int temp = nums[whitePointer];
+                nums[whitePointer] = nums[bluePointer];
+                nums[bluePointer] = temp;
+                bluePointer--;
+                continue;
+            }
+            if(nums[whitePointer]==1){
+                whitePointer++;
+                continue;
+            }
+            if(nums[whitePointer]==0){
+                int temp = nums[whitePointer];
+                nums[whitePointer] = nums[redPointer];
+                nums[redPointer] = temp;
+                whitePointer++;
+                redPointer++;
+                continue;
+            }
+        }
+        
+    }
